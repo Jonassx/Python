647505f78a17d8b0847783ec33aa2976fb200e68
Jonas Leocadio <64425832+Jonassx@users.noreply.github.com>
2023-08-16 14:31:12 -0300

    Delete Lacos_de_Repeticao(WHILE or FOR) directory

--- Lacos_de_Repeticao(WHILE or FOR)/WHILE_infinito.py
+++
@@ -1,8 +0,0 @@
-numero=int(input("Digite um numero: "))
-while numero<100:
-    print("\n" + str(numero))
-#    numero=numero+1
-print("Laço encerrado....")
-
-# \t Acrescenta uma tabulação antes do numero
-# \n Acrescenta uma quebra de linha
